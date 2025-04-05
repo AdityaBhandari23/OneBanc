c0ccae6e6e88e48624e5ae6d11224b6de99e73ae
Aditya <gopalaaditya5@gmail.com>
2025-04-05 16:19:10 +0530

    Updated settings.txt

--- creditcard_normalizer/settings.py
+++ creditcard_normalizer/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+69-inr3)=9_ae*marrvvjp@htex3mk_vxvxlkd90ulsk-di4b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onebanc.onrender.com']
 
 
 # Application definition
